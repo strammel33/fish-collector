fee8d441c970e9b2959ad667017e518737e1d532
Suzanne <trammelsuzanne@gmail.com>
2023-05-26 15:05:01 -0400

    basic about and index views

---
+++ README.md
@@ -0,0 +1,4 @@
+icons:
+<a href="https://www.flaticon.com/free-icons/fish" title="fish icons">Fish icons created by Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/fish" title="fish icons">Fish icons created by Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/puffer-fish" title="puffer fish icons">Puffer fish icons created by Aranagraphics - Flaticon</a>
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Add the Cat class & list and view function below the imports
+class Fish:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, species, description, age):
+    self.name = name
+    self.species = species
+    self.description = description
+    self.age = age
+
+fishes = [
+  Fish('Bubbles', 'guppy', 'Kinda rude.', 3),
+  Fish('Silver', 'tuna', 'Looks like a turtle.', 0),
+  Fish('CheezeIt', 'goldfish', 'likes cheese', 4),
+  Fish('Mr. Pickles', 'barracuda', 'eats everyone else', 6)
+]
+
+
+def home(request):
+  return HttpResponse('<h1>hello fishies</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def fish_index(request):
+  return render(request, 'fishes/index.html', { 'fishes': fishes})
