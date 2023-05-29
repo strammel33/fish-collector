10d359aa159894cb2cf98144c3dcebcfe2cf97a7
Suzanne <trammelsuzanne@gmail.com>
2023-05-29 13:51:01 -0400

    add update fish functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
   path('fishes/', views.fish_index, name='fish-index'),
   path('fishes/<int:fish_id>/', views.fish_detail, name='fish-detail'),
   path('fishes/create/', views.FishCreate.as_view(), name='fish-create'),
+  path('fishes/<int:pk>/update/', views.FishUpdate.as_view(), name='fish-update'),
+  path('fishes/<int:pk>/delete/', views.FishDelete.as_view(), name='fish-delete'),
   
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Fish
 
 # Create your views here.
@@ -21,3 +21,11 @@ def fish_detail(request, fish_id):
 class FishCreate(CreateView):
   model = Fish
   fields = '__all__'
+
+class FishUpdate(UpdateView):
+  model = Fish
+  fields = ['species', 'description', 'age']
+
+class FishDelete(DeleteView):
+  model = Fish
+  success_url = '/fishes/'
