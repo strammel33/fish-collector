c0b1cd1750714741047c155a055bb55cceea28cc
Suzanne <trammelsuzanne@gmail.com>
2023-05-29 13:08:05 -0400

    make fish details view with terrible css

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('fishes/', views.fish_index, name='fish-index')
+  path('fishes/', views.fish_index, name='fish-index'),
+  path('fishes/<int:fish_id>/', views.fish_detail, name='fish-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Fish
 
 # Create your views here.
-# Add the Cat class & list and view function below the imports
-class Fish:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-fishes = [
-  Fish('Bubbles', 'guppy', 'Kinda rude.', 3),
-  Fish('Silver', 'tuna', 'Looks like a turtle.', 0),
-  Fish('CheezeIt', 'goldfish', 'likes cheese', 4),
-  Fish('Mr. Pickles', 'barracuda', 'eats everyone else', 6)
-]
-
 
 def home(request):
-  return HttpResponse('<h1>hello fishies</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def fish_index(request):
-  return render(request, 'fishes/index.html', { 'fishes': fishes})
+  fishes = Fish.objects.all()
+  return render(request, 'fishes/index.html', { 'fishes': fishes })
+
+def fish_detail(request, fish_id):
+  fish = Fish.objects.get(id=fish_id)
+  return render(request, 'fishes/detail.html', { 'fish':fish })
