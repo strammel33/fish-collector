37693b206d614168b0d9fb1b2bfd99e92fee73ee
Suzanne <trammelsuzanne@gmail.com>
2023-05-30 14:52:54 -0400

    make candy model

--- main_app/models.py
+++ main_app/models.py
@@ -38,3 +38,14 @@ class Exercise(models.Model):
   
   class Meta:
     ordering = ['-date']
+
+class Candy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+      return reverse("candy-detail", kwargs={"pk": self.id})
+  
