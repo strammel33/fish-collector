e8019aaedc996303bad2f556a587c77957b3be45
Suzanne <trammelsuzanne@gmail.com>
2023-05-30 17:24:09 -0400

    implement create a candy functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Fish
+from .models import Fish, Candy
 from .forms import ExerciseForm
 
 # Create your views here.
@@ -40,3 +41,15 @@ class FishUpdate(UpdateView):
 class FishDelete(DeleteView):
   model = Fish
   success_url = '/fishes/'
+
+class CandyCreate(CreateView):
+  model = Candy
+  fields = '__all__'
+
+class CandyList(ListView):
+  model = Candy
+
+
+class CandyDetail(DetailView):
+  model = Candy
+
