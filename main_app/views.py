7b1ed5e0613feea33937b690b5fdf509fca04eea
Suzanne <trammelsuzanne@gmail.com>
2023-05-31 15:53:32 -0400

    implement sign up functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 from .models import Fish, Candy
 from .forms import ExerciseForm
 
@@ -70,3 +72,17 @@ class CandyUpdate(UpdateView):
 class CandyDelete(DeleteView):
   model = Candy
   success_url = '/candies/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
