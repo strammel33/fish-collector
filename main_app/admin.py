31572234742854479f9ad623e948036cdf7eadc2
Suzanne <trammelsuzanne@gmail.com>
2023-06-01 15:10:49 -0400

    register photo to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Fish, Exercise, Candy
+from .models import Fish, Exercise, Candy, Photo
 
 # Register your models here.
 admin.site.register(Fish)
 admin.site.register(Exercise)
 admin.site.register(Candy)
+admin.site.register(Photo)
